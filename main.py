9aca9e2dba618173b2fbd83230f63acef512ec66
Roy-Prosanto <120961432+Roy-Prosanto@users.noreply.github.com>
2023-01-01 13:02:51 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+
+potato=int(input((" How Many Kg You Want Potato : ")))
+taka=1.99*potato
+print(f' Potato taka: {taka}')
+
+
+fish=int(input(" How Many Kg You Want Fish : "))
+taka1 = 3 * fish
+print(f' Fish taka: {taka1}')
+
+
+ginger = float(input(" How Many Kg You Want Ginger : "))
+taka2 = 2.00 * ginger
+print(f' Fish taka: {taka2}')
+
+
+totomato = int(input(" How Many Kg You Want Totomato : "))
+taka3= 2.99 * totomato
+print(f' Fish taka: {taka3}')
+
+
+print(f"Total goods buy :{potato+ginger+totomato+fish}kg")
+print(f"Total goods Money :{taka3+taka1+taka2+taka}Taka")
